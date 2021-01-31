87980f125493c30b82349112175b31f93ad97edc
AmbarishKapil <kapilambarish@gmail.com>
2021-01-31 11:13:48 +0530

    Added some in-class documentation for AbstractFactory in the client code

--- src/pattern/abstractfactory/Main.java
+++ src/pattern/abstractfactory/Main.java
@@ -1,5 +1,13 @@
 package pattern.abstractfactory;
 
+/**
+ * This is an implementation of the Abstract factory design pattern
+ *
+ * Here we have a situation where we need to spawn a player, with appropriate weapon and armor
+ * depending on the time of day the mission takes place. Thus we have created a PlayerThemeFactory
+ * as an abstract factory which produces two factories: DayTheme and NightTheme and both of them
+ * return their unique combination of weapon and armor.
+ */
 public class Main {
     public static void main(String[] args) {
         PlayerThemeFactory playerThemeFactory = new DayTheme();
