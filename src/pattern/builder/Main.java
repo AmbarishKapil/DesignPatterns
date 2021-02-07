31eb08dc1ea92da22ad76fa770ab9dca98ce05cc
AmbarishKapil <kapilambarish@gmail.com>
2021-02-07 23:35:38 +0530

    Added context for builder pattern

--- src/pattern/builder/Main.java
+++ src/pattern/builder/Main.java
@@ -1,5 +1,13 @@
 package pattern.builder;
 
+/**
+ * This is an implementation of the builder pattern
+ *
+ * Here we are customizing our main character, we have a ICharacterBuilder interface and
+ * two classes AttireBuilder and BareBodyBuilder for the clothes and body type that implement
+ * our interface. We also have a director class that gives us the default set male and
+ * female main character design.
+ */
 public class Main {
     public static void main(String[] args) {
         BareBodyBuilder bareBodyBuilder = new BareBodyBuilder();
