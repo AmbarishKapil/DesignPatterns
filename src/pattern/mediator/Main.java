a7295b1bd26e56fb5131658963481682c9503924
AmbarishKapil <kapilambarish@gmail.com>
2021-03-14 22:43:32 +0530

    Added javadoc for Mediator pattern

--- src/pattern/mediator/Main.java
+++ src/pattern/mediator/Main.java
@@ -1,5 +1,15 @@
 package pattern.mediator;
 
+/**
+ * This is an implementation of the mediator design pattern
+ *
+ * Here we are implementing the game starting logic. The starting button is not clickable
+ * until GameMode is selected and the appropriate map is loaded. Now the GameMode and MapGraphics
+ * are interdependent, but they don't communicate directly with each, they use the StartGameScene
+ * mediator to communicate.
+ *
+ */
+
 public class Main {
     public static void main(String[] args) {
         GameMode gameMode = new GameMode();
