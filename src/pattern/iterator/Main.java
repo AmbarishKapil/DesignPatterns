4ba7a9a331860fedfee9c5c39d3329fdbd221000
Ambarish Kapil <kapilambarish@gmail.com>
2023-08-27 09:53:23 +0530

    Updated description for Iterator Pattern

--- src/pattern/iterator/Main.java
+++ src/pattern/iterator/Main.java
@@ -12,6 +12,14 @@ import java.util.Scanner;
  * WeaponInventory and WeaponIterator. We also have a Weapons class to store weapon related info.
  * The implementation is like a circular list where the player can move forward, move back or
  * lock on the current weapon.
+ *
+ * IN THE ITERATOR PATTERN WE DECOUPLE THE ALGORITHM FROM THE CONTAINER. This means that we can store the data in
+ * any data structure, be it trees, graphs, linked lists etc. but we are not concerned with that. We don't want to know
+ * which is the underlying data structure, cause if we do we need to handle the traversal for that specific data structure,
+ * so if we had a tree, we would have to do tree.left() or if it was a linked list we need to list.next(), which is
+ * introducing unneeded complexity. What we instead do is say, WE ONLY SPEAK THE ITERATOR LANGUAGE. We just know, moveNext()
+ * movePrevious() and giveCurrent(). And how these are implemented is not our concern, the inner implementation can even
+ * change if a better data structure comes along, we aren't concerned at the least because it's not breaking our code.
  */
 public class Main {
     public static void main(String[] args) {
